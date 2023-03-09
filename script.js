041a409b33c00658eda9c2b828dde585ce5dab98
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-03-09 12:35:25 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let displayTime="";
+// console.log('1');
+let time=document.getElementById('timer');
+function timing(){
+    // console.log('2');
+    let times=new Date().toLocaleString();
+    displayTime=times
+    time.innerText=displayTime;
+    time.style.alignItems="center";
+}
+setInterval(timing,1000);
